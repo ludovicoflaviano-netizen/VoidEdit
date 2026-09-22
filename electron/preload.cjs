2a925ba46d8a60fdfd7596441bafceaea0d20eb5
ludovicoflaviano-netizen <ludovico.flaviano@gmail.com>
2026-09-22 14:13:26 +0700

    feat: expose project save and load APIs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -1,1 +1,7 @@
-const {contextBridge,ipcRenderer}=require("electron");contextBridge.exposeInMainWorld("voidEdit",{openMedia:()=>ipcRenderer.invoke("open-media")});
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("voidEdit", {
+  openMedia: () => ipcRenderer.invoke("open-media"),
+  saveProject: project => ipcRenderer.invoke("save-project", project),
+  openProject: () => ipcRenderer.invoke("open-project")
+});
